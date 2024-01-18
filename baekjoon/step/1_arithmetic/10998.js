e1e95375577c7c83f01f7089d9546bb1726b7d9a
seungzzok <ki2684@naver.com>
2024-01-19 00:03:35 +0900

    [baekjoon] 10998

---
+++ baekjoon/step/1_arithmetic/10998.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .split(" ")
+  .map((e) => parseInt(e));
+
+const [a, b] = input;
+
+console.log(a * b);
