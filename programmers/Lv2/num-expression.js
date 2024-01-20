62ec98823615df360e0d62109a2746252d3ed967
seungzzok <ki2684@naver.com>
2024-01-20 15:06:23 +0900

    [programmers] 숫자의 표현 (24.01.20)

---
+++ programmers/Lv2/num-expression.js
@@ -0,0 +1,31 @@
+function solution(n) {
+  let cnt = 0;
+  for (let i = 1; i <= n; i++) {
+    let sum = 0;
+    for (let j = i; j <= n; j++) {
+      sum += j;
+      if (sum === n) {
+        cnt++;
+        break;
+      }
+      if (sum > n) break;
+    }
+  }
+  return cnt;
+}
+
+solution(15);
+
+// 모범답안
+function expressions(num) {
+  var answer = 0;
+  for (var i = 1; i <= num; i++) {
+    if (num % i == 0 && i % 2 == 1) {
+      answer++;
+    }
+  }
+  return answer;
+}
+
+// 메모
+// 걍 이해안됨. 아니 그리고 내꺼 틀릴거 없는데 시간초과로 틀렸다 뜨거든?? 이거 좀 어이없네
